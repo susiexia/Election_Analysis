4323893828c5fe62b0ec107464ecf71600c6d250
susiexia <57109686+susiexia@users.noreply.github.com>
2019-11-28 15:31:23 -0800

    processing

--- PyPoll.py
+++ PyPoll.py
@@ -11,6 +11,14 @@ candidate_options=list()
 counter_counties=dict()
 counter_candidates=dict()
 
+winning_candidate=str()
+winning_candidate_count=0
+winning_candidate_percentage=0
+
+winning_county=str()
+winning_county_count=0
+winning_county_percentage=0
+
 #outputFile.close() only needed with out WITH statement, combine with open() fection
 with open(file_to_load) as election_data_fhand:
 # To do: read and analyze the data here
@@ -33,14 +41,40 @@ with open(file_to_load) as election_data_fhand:
         #histogram of counties
         counter_counties[country_name]=counter_counties.get(country_name,0)+1
 
+
         if candidate_name not in candidate_options:
             candidate_options.append(candidate_name)
-            #begin tracking the candidate counter, init counter as zero
-            counter_candidates[candidate_name]=0
+            #counter_candidates[candidate_name]=0
+# 4. the total number of votes each candidate won
         #histogram of candidates
-        counter_candidates[candidate_name]=counter_candidates.get(candidate_name)+1
+        counter_candidates[candidate_name]=counter_candidates.get(candidate_name,0)+1
 
-        
+for county in counter_counties:
+    county_votes=counter_counties[county]
+    county_percentage = int(county_votes)/int(total_votes)*100
+    print(f"{county}:  {county_percentage :.1f}% ({county_votes:,})\n")
+
+
+# 3. the percentage of votes each candidate won
+for candidate in counter_candidates:
+    vote_percentage = int(counter_candidates[candidate])/int(total_votes)*100
+    print(f"{candidate}: {vote_percentage :.1f}% ({counter_candidates[candidate]:,})\n")
+    # decision statement for winner!!
+    if winning_candidate_count is None or winning_candidate_count < counter_candidates[candidate]:
+        winning_candidate_count=counter_candidates[candidate]
+    if winning_candidate_percentage is None or winning_candidate_percentage < vote_percentage:
+        winning_candidate_percentage = vote_percentage
+        winning_candidate=candidate
+
+# 5.the winner of the election based on popular vote
+winning_candidate_summary=(
+    f'------------------------\n'
+    f'Winner: {winning_candidate}\n'
+    f'Winning Vote Count: {winning_candidate_count:,}\n'
+    f'Winning Percentage: {winning_candidate_percentage:.1f}%\n'
+    f'------------------------\n')
+print(winning_candidate_summary)
+
 print(total_votes)
 
 print(county_options)
@@ -58,7 +92,3 @@ with open(file_to_save, 'w') as outputFile:
   
 
 
-
-# 3. the percentage of votes each candidate won
-# 4. the total number of votes each candidate won
-# 5.the winner of the election based on popular vote
